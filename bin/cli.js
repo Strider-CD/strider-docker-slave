5e039948a0b14e3c392c69ba8ece3f7ed689694e
Jared Forsyth <jared@khanacademy.org>
2014-08-05 22:50:12 -0700

    not pursuing this direction atm. saving

--- bin/cli.js
+++ bin/cli.js
@@ -4,4 +4,14 @@
  * as a JSON eventemitter
  */
 
+var Runner = require('strider-simple-runner')
+
+var emitter = new EE()
+var runner = new Runner(emitter, {});
+
+// yaay mokeypatch it up
+emitter.receive = emitter.emit
+emitter.emit = function () {
+}
+
 
